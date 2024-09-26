6dba6b48458939b0bfc2d04f3456157362b29372
iamPavan17 <iampavan05@gmail.com>
2024-09-26 22:04:32 +0530

    test > app.js

--- users/src/App.test.js
+++ users/src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, within } from "@testing-library/react";
+import user from "@testing-library/user-event";
 
-test('renders learn react link', () => {
+import App from "./App";
+
+test("it can receive a new user and show it on the list", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const nameInput = screen.getByRole("textbox", {
+    name: /name/i,
+  });
+  const emailInput = screen.getByRole("textbox", {
+    name: /email/i,
+  });
+  const button = screen.getByRole("button");
+
+  user.click(nameInput);
+  user.keyboard("Rama");
+  user.click(emailInput);
+  user.keyboard("rama@gmail.com");
+  user.click(button);
+
+  const name = screen.getByRole("cell", { name: "Rama" });
+  const email = screen.getByRole("cell", { name: "rama@gmail.com" });
+
+  expect(name).toBeInTheDocument();
+  expect(email).toBeInTheDocument();
 });
