b9e78f9fab5b595a07f2665e6e60f8d67355657d
iamPavan17 <iampavan05@gmail.com>
2024-10-03 16:00:45 +0530

    another 2 more test

--- codesplain/src/components/repositories/RepositoriesListItem.test.js
+++ codesplain/src/components/repositories/RepositoriesListItem.test.js
@@ -1,4 +1,4 @@
-import { screen, render, act } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import RepositoriesListItem from "./RepositoriesListItem";
 import { MemoryRouter } from "react-router-dom";
 
@@ -45,6 +45,28 @@ test("shows a link to the github homepage for this repository", async function (
     name: /github repository/i,
   });
   expect(linkElement).toHaveAttribute("href", repository.html_url);
+});
+
+test("shows a fileicon with the appropriate icon", async () => {
+  renderComponent();
+
+  const icon = await screen.findByRole("img", {
+    name: /javascript/i,
+  });
+  expect(icon).toHaveClass("js-icon");
+});
+
+test("shows a link to the code editor page", async () => {
+  const { repository } = renderComponent();
+  await screen.findByRole("img", {
+    name: /javascript/i,
+  });
+
+  const link = screen.getByRole("link", {
+    name: new RegExp(repository.owner.login),
+  });
+
+  expect(link).toHaveAttribute("href", `/repositories/${repository.full_name}`);
 });
 
 // function pause() {
