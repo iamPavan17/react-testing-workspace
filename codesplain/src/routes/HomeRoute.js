1c74e178456f90579bf6e9ae9316f972d37a79b5
iamPavan17 <iampavan05@gmail.com>
2024-10-04 11:36:29 +0530

    implementing a fix > data-fetching

--- codesplain/src/routes/HomeRoute.js
+++ codesplain/src/routes/HomeRoute.js
@@ -1,12 +1,15 @@
-import Hero from '../components/Hero';
-import RepositoriesTable from '../components/repositories/RepositoriesTable';
-import useRepositories from '../hooks/useRepositories';
+import Hero from "../components/Hero";
+import RepositoriesTable from "../components/repositories/RepositoriesTable";
+import useRepositories from "../hooks/useRepositories";
 
 function HomeRoute() {
-  const { data: jsRepos } = useRepositories('stars:>10000 language:javascript');
-  const { data: tsRepos } = useRepositories('stars:>10000 language:typescript');
-  const { data: rustRepos } = useRepositories('stars:>10000 language:rust');
-  const { data: goRepos } = useRepositories('stars:>10000 language:go');
+  // Inside useRepositories(), we're calling APIs
+  const { data: jsRepos } = useRepositories("stars:>10000 language:javascript");
+  const { data: tsRepos } = useRepositories("stars:>10000 language:typescript");
+  const { data: rustRepos } = useRepositories("stars:>10000 language:rust");
+  const { data: goRepos } = useRepositories("stars:>10000 language:go");
+  const { data: pythonRepos } = useRepositories("stars:>10000 language:python");
+  const { data: javaRepos } = useRepositories("stars:>10000 language:java");
 
   return (
     <div>
@@ -22,6 +25,11 @@ function HomeRoute() {
         />
         <RepositoriesTable label="Most Popular Rust" repositories={rustRepos} />
         <RepositoriesTable label="Most Popular Go" repositories={goRepos} />
+        <RepositoriesTable
+          label="Most Popular Python"
+          repositories={pythonRepos}
+        />
+        <RepositoriesTable label="Most Popular Java" repositories={javaRepos} />
       </div>
     </div>
   );
