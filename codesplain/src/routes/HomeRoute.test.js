cfdb557337e593dda30d65c0d4835dd0c49b6adf
iamPavan17 <iampavan05@gmail.com>
2024-10-07 22:53:43 +0530

    homeroute test

--- codesplain/src/routes/HomeRoute.test.js
+++ codesplain/src/routes/HomeRoute.test.js
@@ -1,4 +1,4 @@
-import { screen, render } from "@testing-library/react";
+import { screen, render, findAllByRole } from "@testing-library/react";
 import { setupServer } from "msw/node";
 import { rest } from "msw";
 import { MemoryRouter } from "react-router";
@@ -10,14 +10,13 @@ const handlers = [
   rest.get("/api/repositories", (req, res, ctx) => {
     // Our all requests have query string with a letter q in it.
     // And it has some filter criteria.
-    const query = req.url.searchParams.get("q");
-    console.log(query);
+    const language = req.url.searchParams.get("q").split("language:")[1];
 
     return res(
       ctx.json({
         items: [
-          { id: 1, full_name: "test1" },
-          { id: 2, full_name: "test2" },
+          { id: 1, full_name: `${language}_one` }, // Example -> javascript_one
+          { id: 2, full_name: `${language}_two` }, // Example -> javascript_two
         ],
       })
     );
@@ -39,4 +38,37 @@ afterEach(() => {
 // Will execute one time after all tests have executed inside of this file.
 afterAll(() => {
   server.close(); // close the server
+});
+
+test("render two links for each language", async () => {
+  render(
+    <MemoryRouter>
+      <HomeRoute />
+    </MemoryRouter>
+  );
+
+  // Loop over each language, because for each language we're calling seperate APIs.
+  // Overall, 6 APIs are executed
+  const languages = [
+    "javascript",
+    "typescript",
+    "rust",
+    "go",
+    "python",
+    "java",
+  ];
+  for (let language of languages) {
+    // For each language, make sure we see two links
+    const links = await screen.findAllByRole("link", {
+      name: new RegExp(`${language}_`),
+    });
+
+    // Assert that links have the appropriate full_name
+    expect(links).toHaveLength(2);
+    expect(links[0]).toHaveTextContent(`${language}_one`);
+    expect(links[1]).toHaveTextContent(`${language}_two`);
+
+    expect(links[0]).toHaveAttribute("href", `/repositories/${language}_one`);
+    expect(links[1]).toHaveAttribute("href", `/repositories/${language}_two`);
+  }
 });
