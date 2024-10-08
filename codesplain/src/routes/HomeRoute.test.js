c4717b0b9aa808d892e9b74033c2c4935606e9c1
iamPavan17 <iampavan05@gmail.com>
2024-10-08 10:33:07 +0530

    creating reusable handlers

--- codesplain/src/routes/HomeRoute.test.js
+++ codesplain/src/routes/HomeRoute.test.js
@@ -1,44 +1,60 @@
-import { screen, render, findAllByRole } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import { setupServer } from "msw/node";
 import { rest } from "msw";
 import { MemoryRouter } from "react-router";
 import HomeRoute from "./HomeRoute";
+import { createServer } from "../test/server";
 
-const handlers = [
-  // So this function, is going to watch for any request
-  // that is being made by our components.
-  rest.get("/api/repositories", (req, res, ctx) => {
-    // Our all requests have query string with a letter q in it.
-    // And it has some filter criteria.
-    const language = req.url.searchParams.get("q").split("language:")[1];
-
-    return res(
-      ctx.json({
+createServer([
+  {
+    path: "/api/repositories",
+    res: (req) => {
+      const language = req.url.searchParams.get("q").split("language:")[1];
+      return {
         items: [
           { id: 1, full_name: `${language}_one` }, // Example -> javascript_one
           { id: 2, full_name: `${language}_two` }, // Example -> javascript_two
         ],
-      })
-    );
-  }),
-];
+      };
+    },
+  },
+]);
 
-const server = setupServer(...handlers);
+// const handlers = [
+//   // So this function, is going to watch for any request
+//   // that is being made by our components.
+//   rest.get("/api/repositories", (req, res, ctx) => {
+//     // Our all requests have query string with a letter q in it.
+//     // And it has some filter criteria.
+//     const language = req.url.searchParams.get("q").split("language:")[1];
 
-// Will execute one time before all the tests inside of this file.
-beforeAll(() => {
-  server.listen(); // start the server and listen
-});
+//     return res(
+//       ctx.json({
+//         items: [
+//           { id: 1, full_name: `${language}_one` }, // Example -> javascript_one
+//           { id: 2, full_name: `${language}_two` }, // Example -> javascript_two
+//         ],
+//       })
+//     );
+//   }),
+// ];
 
-// Will execute one time after each individual tests is executed inside of this file.
-afterEach(() => {
-  server.resetHandlers(); // reset the handlers to their initial state.
-});
+// const server = setupServer(...handlers);
 
-// Will execute one time after all tests have executed inside of this file.
-afterAll(() => {
-  server.close(); // close the server
-});
+// // Will execute one time before all the tests inside of this file.
+// beforeAll(() => {
+//   server.listen(); // start the server and listen
+// });
+
+// // Will execute one time after each individual tests is executed inside of this file.
+// afterEach(() => {
+//   server.resetHandlers(); // reset the handlers to their initial state.
+// });
+
+// // Will execute one time after all tests have executed inside of this file.
+// afterAll(() => {
+//   server.close(); // close the server
+// });
 
 test("render two links for each language", async () => {
   render(
