ecd71944c58b0fd5d8c0d01762d4efbd05688024
iamPavan17 <iampavan05@gmail.com>
2024-10-21 10:33:19 +0530

    resolved act warning

--- codesplain/src/components/auth/AuthButtons.test.js
+++ codesplain/src/components/auth/AuthButtons.test.js
@@ -3,6 +3,16 @@ import { MemoryRouter } from "react-router";
 import { createServer } from "../../test/server";
 import AuthButtons from "./AuthButtons";
 
+function renderComponent() {
+  return render(
+    <MemoryRouter>
+      <AuthButtons />
+    </MemoryRouter>
+  );
+}
+
+// const pause = () => new Promise((resolve) => setTimeout(resolve, 100));
+
 /* When the user is not signed in */
 describe("when user is not signed in", () => {
   createServer([
@@ -13,28 +23,30 @@ describe("when user is not signed in", () => {
       },
     },
   ]);
-  test("when user is not signed in, sign in and sign up are visible", async () => {
-    console.log("Test 1");
+  test("sign in and sign up are visible", async () => {
+    renderComponent();
+    await screen.findAllByRole("link");
   });
-  test("when user is not signed in, sign out is not visible", async () => {
-    console.log("Test 2");
+  test("sign out is not visible", async () => {
+    renderComponent();
+    await screen.findAllByRole("link");
   });
 });
 
 /* When the user is signed in */
-describe("when user is signed in", () => {
-  createServer([
-    {
-      path: "/api/user",
-      res: () => {
-        return { user: { id: 1, email: "pavan@bambu.co" } };
-      },
-    },
-  ]);
-  test("when user is signed in, sign in and sign up are not visible", async () => {
-    console.log("Test 3");
-  });
-  test("when user is signed in, sign out is visible", async () => {
-    console.log("Test 4");
-  });
-});
+// describe("when user is signed in", () => {
+//   createServer([
+//     {
+//       path: "/api/user",
+//       res: () => {
+//         return { user: { id: 1, email: "rama@mail.co" } };
+//       },
+//     },
+//   ]);
+//   test("sign in and sign up are not visible", async () => {
+//     renderComponent();
+//   });
+//   test("sign out is visible", async () => {
+//     renderComponent();
+//   });
+// });
